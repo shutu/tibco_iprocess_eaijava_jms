42386f92815f7add0001455ebf98088bc30a058a
eric_yang <gelnyang@163.com>
2015-10-22 13:58:28 +0800

    catch exception for jms

--- src/com/tibco/iprocess/eaijava/jms/JmsSender.java
+++ src/com/tibco/iprocess/eaijava/jms/JmsSender.java
@@ -20,52 +20,61 @@ import com.tibco.iprocess.eaijava.jms.domain.ServiceResponse;
  */
 public class JmsSender {
 
-    private static final Log logger = LogFactory.getLog(JmsSender.class);
+  private static final Log logger = LogFactory.getLog(JmsSender.class);
 
-    private static String serverUrl = "tcp://ipejmsserver1:7222,tcp://ipejmsserver2:7222";
+  private static String serverUrl = "tcp://ipejmsserver1:7222,tcp://ipejmsserver2:7222";
 
-    private static String userName = "admin";
+  private static String userName = "admin";
 
-    private static String password = "";
+  private static String password = "";
 
-    static {
-        try {
-            InputStream is = JmsSender.class
-                    .getResourceAsStream("/iprocess_eaijava.properties");
-            Properties properties = new Properties();
-            properties.load(is);
-            String url = properties.getProperty("eaijava.jms.url");
-            if (url != null) {
-                serverUrl = url;
-            }
-            String user = properties.getProperty("eaijava.jms.user");
-            if (user != null) {
-                userName = user;
-            }
-            String pass = properties.getProperty("eaijava.jms.password");
-            if (pass != null) {
-                password = pass;
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+  static {
+    try {
+      InputStream is = JmsSender.class.getResourceAsStream("/iprocess_eaijava.properties");
+      Properties properties = new Properties();
+      properties.load(is);
+      String url = properties.getProperty("eaijava.jms.url");
+      if (url != null) {
+        serverUrl = url;
+      }
+      String user = properties.getProperty("eaijava.jms.user");
+      if (user != null) {
+        userName = user;
+      }
+      String pass = properties.getProperty("eaijava.jms.password");
+      if (pass != null) {
+        password = pass;
+      }
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    public static ServiceResponse sendJms(String queue, ServiceRequest request)
-            throws Exception {
-        String messageString = request.getFormatMessageString();
-        logger.info(queue + " => " + messageString);
-        String responseValue = sendJms(queue, messageString);
-        logger.info(responseValue);
-        ServiceResponse response = new ServiceResponse();
-        response.initMessageFromFormatString(responseValue);
-        return response;
+  public static ServiceResponse sendJms(String queue, ServiceRequest request) throws Exception {
+    String messageString = request.getFormatMessageString();
+    logger.info(queue + " => " + messageString);
+    String responseValue = "S";
+    ServiceResponse response = new ServiceResponse();
+    try {
+      responseValue = sendJms(queue, messageString);
+    } catch (Exception e) {
+      String message = e.getMessage();
+      logger.error("request:" + messageString + "\r\n error:" + message, e);
+      response.setReturnCode(ServiceResponse.RETURNCODE_FAILED);
+      if (message.length() > 50) {
+        message = message.substring(0, 50);
+      }
+      response.setReturnMsg(message);
+      return response;
     }
+    logger.info(responseValue);
+    response.initMessageFromFormatString(responseValue);
+    return response;
+  }
 
-    private static String sendJms(String queue, String requestValue)
-            throws JMSException, InterruptedException {
-        EmsProducer producer = new EmsProducer(serverUrl, userName, password,
-                queue);
-        return producer.send(requestValue);
-    }
+  private static String sendJms(String queue, String requestValue) throws JMSException,
+      InterruptedException {
+    EmsProducer producer = new EmsProducer(serverUrl, userName, password, queue);
+    return producer.send(requestValue);
+  }
 }
--- src/com/tibco/iprocess/eaijava/jms/domain/ServiceResponse.java
+++ src/com/tibco/iprocess/eaijava/jms/domain/ServiceResponse.java
@@ -2,201 +2,204 @@ package com.tibco.iprocess.eaijava.jms.domain;
 
 public class ServiceResponse extends AbstractMessage {
 
-	/** 返回编号(S:调用成功返回成功,F:调用成功返回失败,EXCEPTION:调用异常) */
-	private String returnCode;
+  public static final String RETURNCODE_SUCCESS = "S";
+  public static final String RETURNCODE_FAILED = "F";
 
-	/** 返回信息 */
-	private String returnMsg;
+  /** 返回编号(S:调用成功返回成功,F:调用成功返回失败,EXCEPTION:调用异常) */
+  private String returnCode;
 
-	/** 返回结果(0:不通过，1:通过) */
-	private String returnResult;
+  /** 返回信息 */
+  private String returnMsg;
 
-	private String retValue1;
+  /** 返回结果(0:不通过，1:通过) */
+  private String returnResult;
 
-	private String retValue2;
+  private String retValue1;
 
-	private String retValue3;
+  private String retValue2;
 
-	private String retValue4;
+  private String retValue3;
 
-	private String retValue5;
+  private String retValue4;
 
-	private String retValue6;
+  private String retValue5;
 
-	private String retValue7;
+  private String retValue6;
 
-	private String retValue8;
+  private String retValue7;
 
-	private String retValue9;
+  private String retValue8;
 
-	private String retValue10;
+  private String retValue9;
 
-	/*-------------------------------------*/
-	public String getFormatMessageString() {
-		StringBuffer buffer = new StringBuffer();
-		addFormatField(buffer, returnCode);
-		addFormatField(buffer, returnMsg);
-		addFormatField(buffer, returnResult);
-		addFormatField(buffer, retValue1);
-		addFormatField(buffer, retValue2);
-		addFormatField(buffer, retValue3);
-		addFormatField(buffer, retValue4);
-		addFormatField(buffer, retValue5);
-		addFormatField(buffer, retValue6);
-		addFormatField(buffer, retValue7);
-		addFormatField(buffer, retValue8);
-		addFormatField(buffer, retValue9);
-		addFormatField(buffer, retValue10);
-		return buffer.toString();
-	}
+  private String retValue10;
 
-	public void initMessageFromFormatString(String s) {
-		String[] arr = s.split(SPLITTER);
-		if (arr.length > 0) {
-			returnCode = arr[0];
-		}
-		if (arr.length > 1) {
-			returnMsg = arr[1];
-		}
-		if (arr.length > 2) {
-			returnResult = arr[2];
-		}
-		if (arr.length > 3) {
-			retValue1 = arr[3];
-		}
-		if (arr.length > 4) {
-			retValue2 = arr[4];
-		}
-		if (arr.length > 5) {
-			retValue3 = arr[5];
-		}
-		if (arr.length > 6) {
-			retValue4 = arr[6];
-		}
-		if (arr.length > 7) {
-			retValue5 = arr[7];
-		}
-		if (arr.length > 8) {
-			retValue6 = arr[8];
-		}
-		if (arr.length > 9) {
-			retValue7 = arr[9];
-		}
-		if (arr.length > 10) {
-			retValue8 = arr[10];
-		}
-		if (arr.length > 11) {
-			retValue9 = arr[11];
-		}
-		if (arr.length > 12) {
-			retValue10 = arr[12];
-		}
-	}
+  /*-------------------------------------*/
+  public String getFormatMessageString() {
+    StringBuffer buffer = new StringBuffer();
+    addFormatField(buffer, returnCode);
+    addFormatField(buffer, returnMsg);
+    addFormatField(buffer, returnResult);
+    addFormatField(buffer, retValue1);
+    addFormatField(buffer, retValue2);
+    addFormatField(buffer, retValue3);
+    addFormatField(buffer, retValue4);
+    addFormatField(buffer, retValue5);
+    addFormatField(buffer, retValue6);
+    addFormatField(buffer, retValue7);
+    addFormatField(buffer, retValue8);
+    addFormatField(buffer, retValue9);
+    addFormatField(buffer, retValue10);
+    return buffer.toString();
+  }
 
-	/*-------------------------------------*/
+  public void initMessageFromFormatString(String s) {
+    String[] arr = s.split(SPLITTER);
+    if (arr.length > 0) {
+      returnCode = arr[0];
+    }
+    if (arr.length > 1) {
+      returnMsg = arr[1];
+    }
+    if (arr.length > 2) {
+      returnResult = arr[2];
+    }
+    if (arr.length > 3) {
+      retValue1 = arr[3];
+    }
+    if (arr.length > 4) {
+      retValue2 = arr[4];
+    }
+    if (arr.length > 5) {
+      retValue3 = arr[5];
+    }
+    if (arr.length > 6) {
+      retValue4 = arr[6];
+    }
+    if (arr.length > 7) {
+      retValue5 = arr[7];
+    }
+    if (arr.length > 8) {
+      retValue6 = arr[8];
+    }
+    if (arr.length > 9) {
+      retValue7 = arr[9];
+    }
+    if (arr.length > 10) {
+      retValue8 = arr[10];
+    }
+    if (arr.length > 11) {
+      retValue9 = arr[11];
+    }
+    if (arr.length > 12) {
+      retValue10 = arr[12];
+    }
+  }
 
-	public String getReturnCode() {
-		return returnCode;
-	}
+  /*-------------------------------------*/
 
-	public void setReturnCode(String returnCode) {
-		this.returnCode = returnCode;
-	}
+  public String getReturnCode() {
+    return returnCode;
+  }
 
-	public String getReturnMsg() {
-		return returnMsg;
-	}
+  public void setReturnCode(String returnCode) {
+    this.returnCode = returnCode;
+  }
 
-	public void setReturnMsg(String returnMsg) {
-		this.returnMsg = returnMsg;
-	}
+  public String getReturnMsg() {
+    return returnMsg;
+  }
 
-	public String getReturnResult() {
-		return returnResult;
-	}
+  public void setReturnMsg(String returnMsg) {
+    this.returnMsg = returnMsg;
+  }
 
-	public void setReturnResult(String returnResult) {
-		this.returnResult = returnResult;
-	}
+  public String getReturnResult() {
+    return returnResult;
+  }
 
-	public String getRetValue1() {
-		return retValue1;
-	}
+  public void setReturnResult(String returnResult) {
+    this.returnResult = returnResult;
+  }
 
-	public void setRetValue1(String retValue1) {
-		this.retValue1 = retValue1;
-	}
+  public String getRetValue1() {
+    return retValue1;
+  }
 
-	public String getRetValue2() {
-		return retValue2;
-	}
+  public void setRetValue1(String retValue1) {
+    this.retValue1 = retValue1;
+  }
 
-	public void setRetValue2(String retValue2) {
-		this.retValue2 = retValue2;
-	}
+  public String getRetValue2() {
+    return retValue2;
+  }
 
-	public String getRetValue3() {
-		return retValue3;
-	}
+  public void setRetValue2(String retValue2) {
+    this.retValue2 = retValue2;
+  }
 
-	public void setRetValue3(String retValue3) {
-		this.retValue3 = retValue3;
-	}
+  public String getRetValue3() {
+    return retValue3;
+  }
 
-	public String getRetValue4() {
-		return retValue4;
-	}
+  public void setRetValue3(String retValue3) {
+    this.retValue3 = retValue3;
+  }
 
-	public void setRetValue4(String retValue4) {
-		this.retValue4 = retValue4;
-	}
+  public String getRetValue4() {
+    return retValue4;
+  }
 
-	public String getRetValue5() {
-		return retValue5;
-	}
+  public void setRetValue4(String retValue4) {
+    this.retValue4 = retValue4;
+  }
 
-	public void setRetValue5(String retValue5) {
-		this.retValue5 = retValue5;
-	}
+  public String getRetValue5() {
+    return retValue5;
+  }
 
-	public String getRetValue6() {
-		return retValue6;
-	}
+  public void setRetValue5(String retValue5) {
+    this.retValue5 = retValue5;
+  }
 
-	public void setRetValue6(String retValue6) {
-		this.retValue6 = retValue6;
-	}
+  public String getRetValue6() {
+    return retValue6;
+  }
 
-	public String getRetValue7() {
-		return retValue7;
-	}
+  public void setRetValue6(String retValue6) {
+    this.retValue6 = retValue6;
+  }
 
-	public void setRetValue7(String retValue7) {
-		this.retValue7 = retValue7;
-	}
+  public String getRetValue7() {
+    return retValue7;
+  }
 
-	public String getRetValue8() {
-		return retValue8;
-	}
+  public void setRetValue7(String retValue7) {
+    this.retValue7 = retValue7;
+  }
 
-	public void setRetValue8(String retValue8) {
-		this.retValue8 = retValue8;
-	}
+  public String getRetValue8() {
+    return retValue8;
+  }
 
-	public String getRetValue9() {
-		return retValue9;
-	}
+  public void setRetValue8(String retValue8) {
+    this.retValue8 = retValue8;
+  }
 
-	public void setRetValue9(String retValue9) {
-		this.retValue9 = retValue9;
-	}
+  public String getRetValue9() {
+    return retValue9;
+  }
 
-	public String getRetValue10() {
-		return retValue10;
-	}
+  public void setRetValue9(String retValue9) {
+    this.retValue9 = retValue9;
+  }
 
-	public void setRetValue10(String retValue10) {
-		this.retValue10 = retValue10;
-	}
+  public String getRetValue10() {
+    return retValue10;
+  }
+
+  public void setRetValue10(String retValue10) {
+    this.retValue10 = retValue10;
+  }
 
 }
